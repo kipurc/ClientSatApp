a5b42b9e3b4e99eef4fbb04e8f2da41a41b51489
pmaridu <pmaridu@us.ibm.com>
2014-04-24 15:18:03 -0400

    Update the develop stream with refactoring for BlueListApplication. Ignore the configuration.json (no longer used).

--- src/com/ibm/bluelist/BlueListApplication.java
+++ src/com/ibm/bluelist/BlueListApplication.java
@@ -23,12 +23,13 @@ import java.util.Properties;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.ibm.mobile.services.core.IBMBaaS;
-import com.ibm.mobile.services.data.IBMDataService;
+import com.ibm.mobile.services.data.IBMData;
 
 public final class BlueListApplication extends Application {
 	public static final int EDIT_ACTIVITY_RC = 1;
@@ -42,24 +43,6 @@ public final class BlueListApplication extends Application {
 			@Override
 			public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
 				Log.d(CLASS_NAME, "Activity created: " + activity.getLocalClassName());
-				// Read from a properties file
-				Properties props = new java.util.Properties();
-				try {
-					AssetManager assetManager = activity.getAssets();					
-					props.load(assetManager.open(BAAS_PROPS_FILE));
-					Log.i(CLASS_NAME, "Found configuration file: " + BAAS_PROPS_FILE);
-				} catch (FileNotFoundException e) {
-					Log.e(CLASS_NAME, "The baas.properties file was not found.", e);
-				} catch (IOException e) {
-					Log.e(CLASS_NAME, "The baas.properties file could not be read properly.", e);
-				}
-				Log.i(CLASS_NAME, "Application ID is: " + props.getProperty(APP_ID));
-				// initialize the IBM core backend-as-a-service
-			    IBMBaaS.initializeSDK(activity, props.getProperty(APP_ID));
-			    // initialize the IBM Data Service
-			    IBMDataService.initializeService();
-			    // register Item data specialization
-			    Item.registerSpecialization(Item.class);
 			}
 			@Override
 			public void onActivityStarted(Activity activity) {
@@ -92,7 +75,25 @@ public final class BlueListApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		itemList = new ArrayList<Item>();
-	    //Item.registerSpecialization(Item.class);
+		// Read from properties file
+		Properties props = new java.util.Properties();
+		Context context = getApplicationContext();
+		try {
+			AssetManager assetManager = context.getAssets();
+			props.load(assetManager.open(BAAS_PROPS_FILE));
+			Log.i(CLASS_NAME, "Found configuration file: " + BAAS_PROPS_FILE);
+		} catch (FileNotFoundException e) {
+			Log.e(CLASS_NAME, "The baas.properties file was not found.", e);
+		} catch (IOException e) {
+			Log.e(CLASS_NAME,
+					"The baas.properties file could not be read properly.", e);
+		}
+		// initialize the IBM core backend-as-a-service
+		IBMBaaS.initializeSDK(this, props.getProperty(APP_ID));
+		// initialize the IBM Data Service
+		IBMData.initializeService();
+		// register the Item Specialization
+		Item.registerSpecialization(Item.class);
 	}
 	
 	/**
